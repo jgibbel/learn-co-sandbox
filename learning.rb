a1d4b9a71f234f6b7935f355b3001824a849c236
James Gibbel <jamesgibbel@gmail.com>
2019-08-08 03:32:58 +0000

    Automatically backed up by Learn

---
+++ learning.rb
@@ -0,0 +1,31 @@
+# comments
+greeting = "friendly_greeting"
+case greeting 
+  when "unfriendly_greeting"
+    puts "wack"
+  when "friendly_greeting"
+    puts "yo"
+end
+
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
+
+6.times do
+  puts "this platform has broken"
+end
+
+# loop do
+# break if (blank is TRUE)
+#   does stuff
+# end 
+
+thisShit = 0 
+until thisShit = 20
+  puts "answer my email... this shit ridiculous"
+  counter += 1 
+end
+
